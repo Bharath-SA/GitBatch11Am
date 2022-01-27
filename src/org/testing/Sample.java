c2e0e9103d4c0b92243183aedb766cafbd8f540a
Bharath-SA <samcannonpix@gmail.com>
2022-01-27 12:47:48 +0530

    Amirtha added her code

--- src/org/testing/Sample.java
+++ src/org/testing/Sample.java
@@ -1,13 +1,15 @@
 package org.testing;
 
 public class Sample {
+	
+	private void m1() {
+		System.out.println("Am method");
+	}
 
 	public static void main(String[] args) {
 		
 		System.out.println("code written");
 		
-		System.out.println("line2");
-		
-		System.out.println("line 3");
+	
 	}
 }
